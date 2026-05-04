72383093d09b3f8b8b4644879b786a36aba37cd9
json-lk <91793362+json-lk@users.noreply.github.com>
2026-05-04 23:56:35 +0000

    Update mainpage_script.js

--- Public/mainpage_script.js
+++ Public/mainpage_script.js
@@ -118,6 +118,22 @@ function updateAccountButton() {
 
 // Ask the server the moment we connect
 socket.on('connect', () => {
+    console.log("Connected to Render. Checking session...");
+    socket.emit('checkSession'); // Custom event we discussed
+});
+
+socket.on('sessionRestore', (data) => {
+    if (data.user) {
+        // SUCCESS: The user is remembered by Supabase
+        localStorage.setItem('currentUser', JSON.stringify(data.user));
+        updateAccountButton(); // Your UI helper
+    } else {
+        // FAIL: No session found, redirect to login
+        window.location.href = 'index.html';
+    }
+});
+
+socket.on('connect', () => {
     socket.emit('checkAuthStatus');
 });
 
